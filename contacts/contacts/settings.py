fe4b138f4b3031a68f53c58876ee5065fbba8b2b
Samuel James <samuel.james@andela.com>
2015-08-22 17:18:47 +0100

    admin login tested

--- contacts/contacts/settings.py
+++ contacts/contacts/settings.py
@@ -66,9 +66,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/lagos'
 
 USE_I18N = True
 
--- ft/tests.py
+++ ft/tests.py
@@ -10,10 +10,19 @@ class AdminTest(LiveServerTestCase):
         self.browser.quit()
 
     def test_admin_site(self):
-        # user opens web browser, navigates to admin page
+    # user opens web browser, navigates to admin page
         self.browser.get(self.live_server_url + '/admin/')
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
+        # users types in username and passwords and presses enter
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('andela')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('andela')
+        password_field.send_keys(Keys.RETURN)
+        # login credentials are correct, and the user is redirected to the main admin page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
 
 
 '''
