e3b1f574581a4af8144e919943597cca904ee80e
Samuel James <samuel.james@andela.com>
2015-08-24 00:09:38 +0100

    adminsite test added again

--- ft/tests.py
+++ ft/tests.py
@@ -1,3 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from django.test import LiveServerTestCase
+
 class UserContactTest(LiveServerTestCase):
 
     def setUp(self):
@@ -6,6 +11,29 @@ class UserContactTest(LiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+
+    def test_admin_site(self):
+    # user opens web browser, navigates to admin page
+        self.browser.get(self.live_server_url + '/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+        # users types in username and passwords and presses enter
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('andela')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('andela')
+        password_field.send_keys(Keys.RETURN)
+        # login credentials are correct, and the user is redirected to the main admin page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+        # user clicks on the Users link
+        #user_link = self.browser.find_elements_by_link_text('Users')
+        #user_link[0].click()
+        # user verifies that user live@forever.com is present
+        #body = self.browser.find_element_by_tag_name('body')
+        #self.assertIn('live@forever.com', body.text)
 
     def test_create_contact(self):
         # user opens web browser, navigates to home page
